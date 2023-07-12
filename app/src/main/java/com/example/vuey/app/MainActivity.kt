cf80968cdcd2d48c34ee519d8fc2d0cb68a9c8e8
Michał F <kfjdjdkh@gmail.com>
2023-07-12 21:41:30 +0200

    change names, domain folder, remove some code, remove firebase notifications

--- app/src/main/java/com/example/vuey/app/MainActivity.kt
+++ app/src/main/java/com/example/vuey/app/MainActivity.kt
@@ -1,51 +1,17 @@
 package com.example.vuey.app
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.vuey.R
 import com.example.vuey.databinding.ActivityMainBinding
-import com.example.vuey.util.Constants
 import dagger.hilt.android.AndroidEntryPoint
 
-@RequiresApi(Build.VERSION_CODES.N_MR1)
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
-    private val requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission(),
-    ) { isGranted: Boolean ->
-        if (isGranted) {
-
-        } else {
-
-        }
-    }
-
-    private fun askNotificationPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            if (ContextCompat.checkSelfPermission(
-                    this,
-                    android.Manifest.permission.POST_NOTIFICATIONS
-                ) ==
-                PackageManager.PERMISSION_GRANTED
-            ) {
-
-            } else {
-                requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
-            }
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,22 +22,5 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bottomNavigation.setupWithNavController(navController)
-
-        askNotificationPermission()
-        createNotificationChannel()
-    }
-
-    private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val fcmChannel = NotificationChannel(
-                Constants.CHANNEL_ID,
-                "FCM_Channel",
-                NotificationManager.IMPORTANCE_HIGH
-            )
-
-            val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-
-            manager.createNotificationChannel(fcmChannel)
-        }
     }
 }
