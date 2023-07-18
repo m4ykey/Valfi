ed3b731a4587fb9fa0ef276b37982d15e60f330c
Michał F <kfjdjdkh@gmail.com>
2023-07-18 19:17:38 +0200

    remove unnecessary code

--- app/src/main/java/com/example/vuey/feature_movie/presentation/SearchMovieFragment.kt
+++ app/src/main/java/com/example/vuey/feature_movie/presentation/SearchMovieFragment.kt
@@ -2,11 +2,10 @@ package com.example.vuey.feature_movie.presentation
 
 import android.os.Bundle
 import android.os.Handler
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -55,32 +54,24 @@ class SearchMovieFragment : Fragment() {
 
     private fun searchMovie() {
         with(binding) {
-            etSearch.addTextChangedListener(object : TextWatcher {
+            etSearch.addTextChangedListener {
+                val searchHandler = Handler()
+                searchHandler.removeCallbacksAndMessages(null)
 
-                private var searchHandler = Handler()
+                val searchMovie = etSearch.text.toString()
 
-                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-
-                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-
-                override fun afterTextChanged(s: Editable?) {
-                    searchHandler.removeCallbacksAndMessages(null)
-
-                    val searchMovie = etSearch.text.toString()
-
-                    if (searchMovie.isNotEmpty()) {
-                        progressBar.visibility = View.VISIBLE
-                        searchHandler.postDelayed({
-                            lifecycleScope.launch {
-                                viewModel.searchMovie(searchMovie)
-                            }
-                            progressBar.visibility = View.GONE
-                        }, 500)
-                    } else {
+                if (searchMovie.isNotEmpty()) {
+                    progressBar.visibility = View.VISIBLE
+                    searchHandler.postDelayed({
+                        lifecycleScope.launch {
+                            viewModel.searchMovie(searchMovie)
+                        }
                         progressBar.visibility = View.GONE
-                    }
+                    }, 500)
+                } else {
+                    progressBar.visibility = View.GONE
                 }
-            })
+            }
         }
     }
 
