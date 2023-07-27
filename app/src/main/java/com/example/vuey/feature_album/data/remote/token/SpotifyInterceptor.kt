2b20a14e72969c5dfd17f3f26951b9591613ba73
Michał F <kfjdjdkh@gmail.com>
2023-07-27 22:01:57 +0200

    improved SpotifyInterceptor

--- app/src/main/java/com/example/vuey/feature_album/data/remote/token/SpotifyInterceptor.kt
+++ app/src/main/java/com/example/vuey/feature_album/data/remote/token/SpotifyInterceptor.kt
@@ -2,6 +2,7 @@ package com.example.vuey.feature_album.data.remote.token
 
 import android.content.SharedPreferences
 import android.util.Base64
+import androidx.core.content.edit
 import com.example.vuey.BuildConfig.SPOTIFY_CLIENT_ID
 import com.example.vuey.BuildConfig.SPOTIFY_CLIENT_SECRET
 import com.example.vuey.feature_album.data.remote.api.AuthApi
@@ -16,17 +17,32 @@ class SpotifyInterceptor @Inject constructor(
 ) : Interceptor {
 
     private val accessTokenKey = "access_token"
+    private val expireTimeKey = "expire_time"
 
     override fun intercept(chain: Interceptor.Chain): Response = runBlocking {
         val request = chain.request()
 
-        val accessToken = sharedPreferences.getString(accessTokenKey, null) ?: getAccessToken()
+        var accessToken = sharedPreferences.getString(accessTokenKey, null)
+        var expireTime = sharedPreferences.getLong(expireTimeKey, 0L)
+
+        if (accessToken == null || System.currentTimeMillis() > expireTime) {
+            accessToken = getAccessToken()
+            expireTime = System.currentTimeMillis() + 3600 * 1000 // 3600 seconds in milliseconds (1hour)
+            saveAccessToken(accessToken, expireTime)
+        }
 
         val newRequest = request.newBuilder()
             .addHeader("Authorization", "Bearer $accessToken")
             .build()
 
         chain.proceed(newRequest)
+    }
+
+    private fun saveAccessToken(accessToken: String, expireTime: Long) {
+        sharedPreferences.edit {
+            putString(accessTokenKey, accessToken)
+            putLong(expireTimeKey, expireTime)
+        }
     }
 
     suspend fun getAccessToken(): String {
