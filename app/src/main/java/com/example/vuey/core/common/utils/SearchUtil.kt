4ccc502805c4073f857b77cb48ba682094a175f0
Michał F <michalfr1601@gmail.com>
2023-08-10 19:43:53 +0200

    removed unnecessary code

--- app/src/main/java/com/example/vuey/core/common/utils/SearchUtil.kt
+++ app/src/main/java/com/example/vuey/core/common/utils/SearchUtil.kt
@@ -1,7 +1,6 @@
 package com.example.vuey.core.common.utils
 
 import com.example.vuey.feature_album.data.remote.model.spotify.album.Album
-import com.example.vuey.feature_movie.data.remote.model.MovieList
 import java.util.Locale
 
 object SearchUtil {
@@ -14,22 +13,6 @@ object SearchUtil {
 
         for (i in 0 until minOf(albumTitle.length, queryLowercase.length)) {
             if (albumTitle[i] == queryLowercase[i]) {
-                matchingCharacters++
-            }
-        }
-
-        return matchingCharacters.toDouble() / maxLength
-    }
-
-    fun calculateMovieMatchingScore(movie: MovieList, query : String) : Double {
-        val movieTitle = movie.title.lowercase(Locale.ROOT)
-        val queryLowercase = query.lowercase(Locale.ROOT)
-
-        val maxLength = maxOf(movieTitle.length, queryLowercase.length)
-        var matchingCharacters = 0
-
-        for (i in 0 until minOf(movieTitle.length, queryLowercase.length)) {
-            if (movieTitle[i] == queryLowercase[i]) {
                 matchingCharacters++
             }
         }
