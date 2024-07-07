683e191b9f34199f7af5378aba042ffe5d1efcb2
Michał F <kfjdjdkh@gmail.com>
2024-07-07 21:55:40 +0200

    solved error with getting access token to spotify api

---
+++ album/data/src/main/java/com/m4ykey/data/remote/interceptor/IsInternetAvailable.kt
@@ -0,0 +1,12 @@
+package com.m4ykey.data.remote.interceptor
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
+fun isInternetAvailable(context : Context) : Boolean {
+    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val activeNetwork = connectivityManager.activeNetwork
+    val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork)
+    return networkCapabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) == true
+}
